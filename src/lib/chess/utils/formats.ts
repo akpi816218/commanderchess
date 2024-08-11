10b3ba7adb802a8d688833b64105f9bb9d67f314
Akhil Pillai <gh@akpi.is-a.dev>
2024-08-11 13:23:42 -0700

    fmt

--- src/lib/chess/utils/formats.ts
+++ src/lib/chess/utils/formats.ts
@@ -1,6 +1,7 @@
 import { Commander } from '@/lib/chess/types';
 
-export function fmtCommanderName(commander: Commander) : string {
+export function fmtCommanderName(commander: Commander): string {
 	if (commander === Commander.Castle) return 'Castle, the Original';
-	else return `${Object.keys(Commander).find(v => v === commander)}, Commander of ${commander}`
+	else
+		return `${Object.keys(Commander).find(v => v === commander)}, Commander of ${commander}`;
 }
